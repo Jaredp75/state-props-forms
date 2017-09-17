f6ed0ebe59e8afeac95150f58565a0cd680c0e58
Jaredp75 <jaredp75@hotmail.com>
2017-09-17 17:02:01 -0400

    Another commit

--- state-props-forms/src/components/App.js
+++ state-props-forms/src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//import './src/styles/App.css';
+import NavBar from '../components/NavBar.js';
+import PlayList from '../components/PlayList.js';
+import PlayListForm from '../components/PlayListForm.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
